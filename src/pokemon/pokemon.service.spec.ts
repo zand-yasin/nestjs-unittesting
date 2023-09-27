231209d7397de1a0a4126efefb8d9af03d1b235c
zand-yasin <zand.kurdy@gmail.com>
2023-09-27 13:44:14 +0300

    Mocking Pokemon Service

--- src/pokemon/pokemon.service.spec.ts
+++ src/pokemon/pokemon.service.spec.ts
@@ -1,21 +1,78 @@
-import { HttpModule } from '@nestjs/axios';
+import { HttpModule, HttpService } from '@nestjs/axios';
+import {
+  BadRequestException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { PokemonService } from './pokemon.service';
+import { DeepMocked, createMock } from '@golevelup/ts-jest';
 
 describe('PokemonService', () => {
-  let pokemonService: PokemonService; // renamed variable to pokemonService
+  let pokemonService: PokemonService;
+  let httpService: DeepMocked<HttpService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [HttpModule],
-      providers: [PokemonService],
+      providers: [
+        PokemonService,
+        {
+          provide: HttpService,
+          useValue: createMock<HttpService>(),
+        },
+      ],
     }).compile();
 
     pokemonService = module.get<PokemonService>(PokemonService);
+    httpService = module.get(HttpService);
   });
 
   it('should be defined', () => {
     expect(pokemonService).toBeDefined();
   });
-});
 
+  describe('getPokemon', () => {
+    it('pokemon ID less than 1 should throw error', async () => {
+      const getPokemon = pokemonService.getPokemon(0);
+
+      await expect(getPokemon).rejects.toBeInstanceOf(BadRequestException);
+    });
+
+    it('pokemon ID greater than 151 should throw error', async () => {
+      const getPokemon = pokemonService.getPokemon(152);
+
+      await expect(getPokemon).rejects.toBeInstanceOf(BadRequestException);
+    });
+
+    it('valid pokemon ID to return the pokemon name', async () => {
+      httpService.axiosRef.mockResolvedValueOnce({
+        data: {
+          species: { name: `bulbasaur` },
+        },
+        headers: {},
+        config: { url: '' },
+        status: 200,
+        statusText: '',
+      });
+
+      const getPokemon = pokemonService.getPokemon(1);
+
+      await expect(getPokemon).resolves.toBe('bulbasaur');
+    });
+
+    it('if Pokemon API response unexpectedly changes, throw an error', async () => {
+      httpService.axiosRef.mockResolvedValueOnce({
+        data: `Unexpected data`,
+        headers: {},
+        config: { url: '' },
+        status: 200,
+        statusText: '',
+      });
+
+      const getPokemon = pokemonService.getPokemon(1);
+
+      await expect(getPokemon).rejects.toBeInstanceOf(
+        InternalServerErrorException,
+      );
+    });
+  });
+});
