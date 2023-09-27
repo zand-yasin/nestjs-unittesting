55f90c419d3e2765b9417c880942cbde21f0ba49
zand-yasin <zand.kurdy@gmail.com>
2023-09-27 08:44:10 +0300

    Add Crud

--- src/tweets/tweets.service.spec.ts
+++ src/tweets/tweets.service.spec.ts
@@ -7,7 +7,6 @@ describe('TweetsService', () => {
 
   // handles any setup work, before test running.
   beforeEach(async () => {
-   
     // access to all the methods inside the TweetsService
     const module: TestingModule = await Test.createTestingModule({
       providers: [TweetsService],
@@ -18,8 +17,42 @@ describe('TweetsService', () => {
 
   // tests...
   it('should be defined', () => {
-    
     // ensuring that the TweetsService is defined.
     expect(service).toBeDefined();
+  });
+
+  describe('createTweet', () => {
+    it('should create tweet', () => {
+      // Arrange
+      service.tweets = [];
+      const payload = 'This is my tweet';
+
+      // Act
+      const tweet = service.createTweet(payload);
+
+      // Assert
+      expect(tweet).toBe(payload);
+      expect(service.tweets).toHaveLength(1);
+    });
+  });
+
+  describe('createTweet', () => {
+    it('should create tweet', () => {
+      // ...
+    });
+
+    it('should prevent tweets created which are over 100 characters', () => {
+      // Arrange
+      const payload =
+        'This is a long tweet over 100 characters This is a long tweet over 100 characters This is a long t...';
+
+      // Act
+      const tweet = () => {
+        return service.createTweet(payload);
+      };
+
+      // Assert
+      expect(tweet).toThrowError();
+    });
   });
 });
